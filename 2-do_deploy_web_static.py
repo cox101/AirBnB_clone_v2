5f0c7d3fb0bb105a317006c4adc1aa5d752c6807
Bobybuu <c43057772.com>
2024-04-04 16:29:19 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,46 @@
 #!/usr/bin/python3
-"""web server distribution"""
-from fabric.api import *
-import os.path
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
 
-env.user = 'ubuntu'
-env.hosts = ["104.196.155.240", "34.74.146.120"]
-env.key_filename = "~/id_rsa"
+
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+
+
+env.hosts = ['35.237.166.125', '54.167.61.201']  # <IP web-01>, <IP web-02>
+# ^ All remote commands must be executed on your both web servers
+# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+
 
 def do_deploy(archive_path):
-    """Deploys the archive to the web servers"""
-    if not os.path.exists(archive_path):
-        print("Archive path does not exist:", archive_path)
-        return False
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False  # Returns False if the file at archive_path doesnt exist
+    filename = archive_path.split('/')[-1]
+    # so now filename is <web_static_2021041409349.tgz>
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    # curr = '/data/web_static/current'
+    tmp = "/tmp/" + filename
 
     try:
-        # Extract the archive filename
-        archive_name = os.path.basename(archive_path)
-        base_name = os.path.splitext(archive_name)[0]
-
-        # Upload the archive to the remote server
-        put(archive_path, "/tmp/{}".format(archive_name))
-
-        # Create the directory structure
-        sudo("mkdir -p /data/web_static/releases/{}/".format(base_name))
-
-        # Unpack the archive
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-             .format(archive_name, base_name))
-
-        # Delete the uploaded archive
-        sudo("rm /tmp/{}".format(archive_name))
-
-        # Move contents to appropriate location
-        sudo("mv /data/web_static/releases/{}/web_static/* "
-             "/data/web_static/releases/{}/"
-             .format(base_name, base_name))
-
-        # Remove empty directory
-        sudo("rm -rf /data/web_static/releases/{}/web_static"
-             .format(base_name))
-
-        # Update symbolic link
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-             .format(base_name))
-
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        # ^ Upload the archive to the /tmp/ directory of the web server
+        run("mkdir -p {}/".format(no_tgz))
+        # Uncompress the archive to the folder /data/web_static/releases/
+        # <archive filename without extension> on the web server
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        # ^ Delete the archive from the web server
+        run("rm -rf /data/web_static/current")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
         return True
-
-    except Exception as e:
-        print("Deployment failed:", e)
+    except:
         return False
-
