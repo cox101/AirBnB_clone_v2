43bdd3eabaa08a9f7c671c9de810e65e980b57ed
cox101 <coxb2659@gmail.com>
2024-04-20 14:43:11 -0700

    Initial commit

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,6 +25,19 @@ def c_text(text):
     return "C " + text.replace("_", " ")
 
 
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Route that displays 'Python ' followed by the value of the text variable"""
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Route that displays 'n is a number' only if n is an integer"""
+    return "{} is a number".format(n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
