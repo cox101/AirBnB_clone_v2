d684560ee170cf3862882828861c7af7a9a17f35
cox101 <coxb2659@gmail.com>
2024-04-21 13:19:19 -0700

    Initial commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Flask web application with specified routes"""
-
+"""Flask framework"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,20 +7,19 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Route that displays a list of all State objects present in DBStorage"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template("7-states_list.html", states=sorted_states)
-
-
 @app.teardown_appcontext
-def teardown(exception):
-    """Teardown method to close the SQLAlchemy session"""
+def teardown_db(exception):
+    """Close the database session"""
     storage.close()
 
 
-if __name__ == "__main__":
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """List all state IDs"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
